9cb85f2f2b6aaae982a0969bb1541f3475ed246c
Alex Beckerman <sonificus@gmail.com>
2020-08-14 10:15:11 -0400

    Shay is my savior

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import Main from 'components/Main'
+import Header from './components/Header'
+import Nav from './components/Nav'
+import Main from './components/Main'
+import Footer from './components/Footer'
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div>
+      <Header />
+      <Nav />
       <Main />
+      <Footer />
     </div>
   );
 }
