f0b4c911d785b766aefccf73814026a33ae85645
Alex Beckerman <sonificus@gmail.com>
2020-08-14 11:39:28 -0400

    added axios, react-router-dom - may accidentally have created another git repo inside client?

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import Header from './components/Header'
 import Nav from './components/Nav'
 import Main from './components/Main'
 import Footer from './components/Footer'
-
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
