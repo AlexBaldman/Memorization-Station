8dbf8bda84f5069f7349792dd2d5fbb6d5148532
Alex Beckerman <sonificus@gmail.com>
2020-08-17 19:13:04 -0400

    update seeds

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,9 +5,16 @@ export default function Home () {
     return (
       <div class='home-content'>
         <h1>Home</h1>
-        <p>The human mind is an amazing thing.  In fact, it is the most complex object in the universe that we are currently aware of, and still have much to learn about how it actually works.</p>
-        <p>One of the most amazing things about the human brain is its ability to store and retrieve information.  But given the sheer volume of information that we
-        take in throughout our lives - or even just over the course of a day - makes it just as important for the brain to clear out space in its "working memory"
+        <p>The human mind is an amazing thing.  In fact, it is the most complex object in the universe that we are 
+        currently aware of, and still have much to learn about how it actually works.</p>
+        <p>One of the most amazing things about the human brain is its ability to store and retrieve information.  
+        Most memory systems involve building a scaffolding of known objects with which to associate with new information to be memorized.
+        These are sometimes known as memory maps, journeys, palaces, etc.</p>
+        
+        <p></p>Basically each is a type of collection of these memory objects
+        visualized to be arranged in a certain order or position (places in your memory palace, or stops along a journey that you're familiar with, etc.)
+        In this way, the memorizer can store other information visualization of people, places, and things that can be used 
+        makes it just as important for the brain to clear out space in its "working memory".
         Studies have shown that when the visual cortex is engaged when creating a memory, it makes the memory more readily available for recall.  This</p>
         <p>One of the main insights about the mind that</p>
       </div>
