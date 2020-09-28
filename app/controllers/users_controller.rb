cf02de39e70084b1c1fa2cec5071de8451b7b712
Alex Beckerman <sonificus@gmail.com>
2020-09-28 17:15:03 -0400

    add password_digest to users controller, users.rb, troubleshooting authentication issue

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -51,6 +51,6 @@ class UsersController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def user_params
-      params.require(:user).permit(:username, :email, :password)
+      params.require(:user).permit(:username, :email, :password, :password_digest)
     end
 end
