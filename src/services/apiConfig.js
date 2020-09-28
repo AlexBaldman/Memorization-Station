10f17137909a25f38aa95e2ff7923825fc841b4a
Alex Beckerman <sonificus@gmail.com>
2020-09-28 16:53:52 -0400

    testing edits in production.rb & apiConfig for CORS & API functionality

--- src/services/apiConfig.js
+++ src/services/apiConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = process.env.NODE_ENV === 'production' ? 'https://cors-anywhere.herokuapp.com/https://git.heroku.com/memorization-station-api.git' : 'http://localhost:3000'
+const baseUrl = process.env.NODE_ENV === 'production' ? 'https://git.heroku.com/memorization-station-api.git' : 'http://localhost:3000'
 
 const api = axios.create({
   baseURL: baseUrl
