6ee1760151715248b27d3580ba02d1027c8d1f80
Alex Beckerman <sonificus@gmail.com>
2021-08-02 21:42:30 -0400

    first commit - troubleshooting authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :authorize_request, except: :create
+  # before_action :authorize_request, except: :create
   before_action :set_user, only: [:index, :show, :update, :destroy]
 
   # GET /users
